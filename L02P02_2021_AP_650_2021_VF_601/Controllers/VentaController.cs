effb858d10809cfd48cfc79437f5285a02826e99
xaldakr <xavieravilaposada@gmail.com>
2024-04-20 14:12:49 -0600

    agregar cosas

---
+++ L02P02_2021_AP_650_2021_VF_601/Controllers/VentaController.cs
@@ -0,0 +1,16 @@
+﻿using L02P02_2021_AP_650_2021_VF_601.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021_AP_650_2021_VF_601.Controllers
+{
+    public class VentaController : Controller
+    {
+        public pedido_encabeza Encabeza;
+        public pedido_deta Detalle;
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
--- L02P02_2021_AP_650_2021_VF_601/Program.cs
+++ L02P02_2021_AP_650_2021_VF_601/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Venta}/{action=Index}/{id?}");
 
 app.Run();
