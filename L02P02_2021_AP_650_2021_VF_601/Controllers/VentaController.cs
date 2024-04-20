3aea1b6adc45aa7eeccdce0458e58d1a84822335
xaldakr <xavieravilaposada@gmail.com>
2024-04-20 15:27:41 -0600

    logic

--- L02P02_2021_AP_650_2021_VF_601/Controllers/VentaController.cs
+++ L02P02_2021_AP_650_2021_VF_601/Controllers/VentaController.cs
@@ -1,5 +1,6 @@
 ﻿using L02P02_2021_AP_650_2021_VF_601.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L02P02_2021_AP_650_2021_VF_601.Controllers
 {
@@ -7,6 +8,12 @@ namespace L02P02_2021_AP_650_2021_VF_601.Controllers
     {
         public pedido_encabeza Encabeza;
         public pedido_deta Detalle;
+
+        private readonly LibreriaContexto _LibreriaContext;
+        public VentaController(LibreriaContexto LibreriaContext)
+        {
+            _LibreriaContext = LibreriaContext;
+        }
         public IActionResult Index()
         {
 
@@ -15,7 +22,20 @@ namespace L02P02_2021_AP_650_2021_VF_601.Controllers
 
         public IActionResult IniciarVenta (cliente Clie)
         {
-            return RedirectToAction("Index"); // Llevalo al siguiente view en vez de index
+            /*
+            _LibreriaContext.Add(Clie);
+            _LibreriaContext.SaveChanges();
+            int idclie = _LibreriaContext.clientes.Select(c => (int?)c.id).Max() ?? 0; // Obtener id de pedido
+            Encabeza = new pedido_encabeza();
+            Encabeza.id_cliente = idclie;
+            Encabeza.total = 0;
+            Encabeza.cantidad_libros = 0;
+            _LibreriaContext.Add(Encabeza);
+            _LibreriaContext.SaveChanges();
+            int idencabeza = _LibreriaContext.pedido_encabezado.Select(c => (int?)c.id).Max() ?? 0; //Obtener el id del encabezado;
+            Encabeza = (pedido_encabeza)(from e in _LibreriaContext.pedido_encabezado where e.id == idencabeza select e);
+            */
+            return RedirectToAction("ListaLibros");
         }
 
         public IActionResult ListaLibros()
